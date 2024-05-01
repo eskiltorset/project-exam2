92d74829776ea575390504ec4f1a1be0ae1e8d60
eskiltorset <44784426+eskiltorset@users.noreply.github.com>
2024-05-01 17:03:02 +0200

    if statement for singleVenue content

--- holidaze/src/pages/SingleVenue/index.jsx
+++ holidaze/src/pages/SingleVenue/index.jsx
@@ -3,6 +3,8 @@ import { useParams } from 'react-router-dom';
 // import { Cartcontext } from '../../context/context';
 // import "./singleProduct.css";
 import "./singleVenue.css";
+import DateRangePicker from '@mui/lab/DateRangePicker';
+
 
 
 
@@ -62,6 +64,7 @@ function Venue() {
 //     price.innerText = `${item.price},-`;
 //   }
 
+if (item.owner.name == localStorage.getItem("loggedInUser")){
   return (
     <div className='vh-100 container row justify-content-center mt-3 col-lg-12'>
       <div className='left-content d-flex item-div col-lg-7'>
@@ -98,6 +101,47 @@ function Venue() {
       </div>
     </div>
   );
+}
+
+else {
+  return (
+    <div className='vh-100 container row justify-content-center mt-3 col-lg-12'>
+      <div className='left-content d-flex item-div col-lg-7'>
+          <div className='m-2 w-100'>
+          <div key={item.id}>
+          <h5 className='mt-2'>{item.name}</h5>
+            <img src={item.media[0].url} alt={item.name}className='w-100'></img>
+            <div className='card-body p-3'>
+              <p className='float-end'>{item.rating}/5&#9733; </p>
+              <h6>{item.location.city}, {item.location.country}</h6>
+              <p>{item.maxGuests} guests</p>
+              <p>{item.description}</p>
+              <div className='avatar-container'>
+                <small>published by<br/></small>
+                <img src={item.owner.avatar.url} alt={item.owner.name}className='rounded-circle'></img>
+                <small><b>{item.owner.name}</b></small>
+              </div>
+              {/* <button onClick={() => dispatch({type: 'ADD', payload: item })} className='btn btn-outline-secondary mb-3 float-end'>Add to cart</button> */}
+            </div>
+          </div>
+          </div>
+      </div>
+      <div className='right-content col-lg-5 mt-5'>
+        <div className='w-100 h-auto border p-3'>
+          <h5>{item.price}kr pr night</h5>
+          <hr class="hr" />
+          <div className='button-container justify-content-around'>
+          <DateRangePicker localeText={{ start: 'Check-in', end: 'Check-out' }} />
+            <button className='w-100 mt-3'>Reserve</button>
+          </div>
+        </div>
+
+      </div>
+    </div>
+  );
+}
+
+  
 }
 
 export default Venue;
